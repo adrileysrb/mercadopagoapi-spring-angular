58ec963b3c01a6e097013d8ddf2d02677f2d0a45
Adriley Samuel Ribeiro Barbosa <asbarbosa@stefanini.com>
2023-08-11 13:15:30 -0300

    Add Spring base project

---
+++ spring-payment/src/test/java/com/example/springpayment/SpringPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
